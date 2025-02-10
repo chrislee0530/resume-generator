a5d3d14bff91d824c79d9c46ef7f0effd252a04f
Chris Lee <clee0530@students.cs.ubc.ca>
2025-02-10 12:35:49 -0800

    Specification for Profile class

--- src/main/model/Profile.java
+++ src/main/model/Profile.java
@@ -3,4 +3,64 @@ package model;
 // Represents the user's profile information including name, contact info, and address
 public class Profile {
 
+    // EFFECTS: constructs a new profile with given information
+    public Profile(String name, String number, String email, String address, String objective) {
+
+    }
+
+    // EFFECTS: returns the user's name
+    public String getName() {
+        return ""; // stub
+    }
+    
+    // EFFECTS: returns the user's phone number
+    public String getNumber() {
+        return ""; // stub
+    }
+
+    // EFFECTS: returns the user's email address
+    public String getEmail() {
+        return ""; // stub
+    }
+
+    // EFFECTS: returns the user's home address
+    public String getAddress() {
+        return ""; // stub
+    }
+
+    // EFFECTS: returns a short reusme objective sentences
+    public String getSummary() {
+        return ""; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates the user's name with given name
+    public void setName(String name) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates the user's phone number with given number
+    public void setNumber(String number) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates the user's email address with given email address
+    public void setEmail(String email) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates the user's home address with given home address
+    public void setAddress(String address) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates the user's resume objective with given objective
+    public void setObjective(String objective) {
+        // stub
+    }
+
 }
--- src/main/model/Resume.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-// Represents a resume that contains multiple sections including profile, experience, education, and skills
-public class Resume {
-
-}

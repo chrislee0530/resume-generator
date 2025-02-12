3909065389f1b317bf075bac66f5ffe0d1037a5f
Chris Lee <clee0530@students.cs.ubc.ca>
2025-02-12 00:58:51 -0800

    Added specification for Experience and Education Class

--- src/main/model/Education.java
+++ src/main/model/Education.java
@@ -1,6 +1,23 @@
 package model;
 
 // Represents education history of the user
-public class Education {
+public class Education extends ResumeSection{
+    String gpa;
+
+    // EFFECTS: Constructs a new experience with institution, locaiton, startYear, endYear, and description
+    public Education(String institution, String location, String startYear, String endYear, String description) {
+		super(institution, location, startYear, endYear, description);
+		
+	}
+
+    // EFFECTS: returns the gpa
+    public String getGpa() {
+        return "";
+    }
+
+    // EFFECTS: sets the gpa with given value
+    public void setGpa(String gpa) {
+        // stub
+    }
 
 }
--- src/main/model/Experience.java
+++ src/main/model/Experience.java
@@ -1,6 +1,24 @@
 package model;
 
 // Represents work experiences of the user
-public class Experience {
+public class Experience extends ResumeSection {
+    String position;
+
+    // EFFECTS: Constructs a new experience with institution, locaiton, startYear, endYear, and description
+    public Experience(String institution, String location, String startYear, String endYear, String description) {
+		super(institution, location, startYear, endYear, description);
+	}
+
+    // EFFECTS: returns the position
+    public String getPosition() {
+        return "";
+    }
+
+    // EFFECTS: sets the position with given value
+    public void setPosition(String position) {
+        // stub
+    }
 
 }
+
+

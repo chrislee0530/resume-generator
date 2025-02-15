6877dae874feae302212acd6cfc4613cd78295a7
Chris Lee <clee0530@students.cs.ubc.ca>
2025-02-15 01:23:08 -0800

    Implemented Experience class

--- src/main/model/Experience.java
+++ src/main/model/Experience.java
@@ -1,21 +1,112 @@
 package model;
 
 // Represents work experiences of the user
-public class Experience extends ResumeSection {
+public class Experience {
     String position;
+    String institution;
+    String location;
+    int startYear;
+    int startMonth;
+    int endYear;
+    int endMonth;
+    String description;
 
-    // EFFECTS: Constructs a new experience with institution, locaiton, startYear, endYear, and description
-    public Experience(String institution, String location, String startYear, String endYear, String description) {
-		super(institution, location, startYear, endYear, description);
-	}
-
-    // EFFECTS: returns the position
-    public String getPosition() {
-        return "";
+    // EFFECTS: Constructs a Experience with position, institution, location, start year, end year, and description
+    public Experience(String position, String institution, String location, 
+    int startYear, int startMonth, int endYear, int endMonth, String description) {
     }
 
-    // EFFECTS: sets the position with given value
+    // EFFECTS: Returns the total duration of the experience
+    public int getTotalDuration() {
+        return 0; //stub
+    }
+
+
+    // EFFECTS: Returns the position name
+    public String getPosition() {
+        return ""; //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the position name to the given value
     public void setPosition(String position) {
+        // stub
+    }
+
+    // EFFECTS: Returns the institution name
+    public String getInstitution() {
+        return ""; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the institution name to the given value
+    public void setInstitution(String institution) {
+        // stub
+    }
+
+    // EFFECTS: Returns the location
+    public String getLocation() {
+        return ""; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the location to the given value
+    public void setLocation(String location) {
+        // stub
+    }
+
+    // EFFECTS: Returns the start year
+    public String getStartYear() {
+        return ""; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the start year to the given value
+    public void setStartYear(String startYear) {
+        // stub
+    }
+
+    // EFFECTS: Returns the start month
+    public String getStartMonth() {
+        return ""; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the start month to startMonth
+    public void setStartMonth(int startMonth) {
+        // stub
+    }
+
+    // EFFECTS: Returns the end year
+    public String getEndYear() {
+        return ""; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the end year to the given value
+    public void setEndYear(String endYear) {
+        // stub
+    }
+
+    // EFFECTS: Returns the end month
+    public String getEndMonth() {
+        return ""; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the end month to endMonth
+    public void setEndMonth(int endMonth) {
+        // stub
+    }
+
+    // EFFECTS: Returns the description of the section
+    public String getDescription() {
+        return ""; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the description to the given value
+    public void setDescription(String description) {
         // stub
     }
 
